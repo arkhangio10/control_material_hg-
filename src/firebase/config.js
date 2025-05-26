663605a95b183718348c38c12e401303aa2ead03
arkhangio10 <44185212+arkhangio10@users.noreply.github.com>
2025-05-26 11:09:23 -0500

    nada que ver

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -8,12 +8,7 @@ import { getStorage } from "firebase/storage";
 // Configuración de Firebase
 // Reemplaza estos valores con los de tu proyecto Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyD2She7vzAfbjkuea1p7vkXXsN8jg9NWO8",
-  authDomain: "control-material-7b4d7.firebaseapp.com",
-  projectId: "control-material-7b4d7",
-  storageBucket: "control-material-7b4d7.firebasestorage.app",
-  messagingSenderId: "650193722396",
-  appId: "1:650193722396:web:69b06b63b083b06710d3d9"
+  
 };
 
 // Inicializar Firebase
