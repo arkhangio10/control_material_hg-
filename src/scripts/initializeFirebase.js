0637ea3d49e5c2a2fd9b0d291e2a013d574dc849
arkhangio10 <44185212+arkhangio10@users.noreply.github.com>
2025-05-26 11:08:45 -0500

    nada que ver

--- src/scripts/initializeFirebase.js
+++ src/scripts/initializeFirebase.js
@@ -13,12 +13,7 @@ import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 
 // Configuración de Firebase (reemplaza con tus credenciales)
 const firebaseConfig = {
-  apiKey: "AIzaSyD2She7vzAfbjkuea1p7vkXXsN8jg9NWO8",
-  authDomain: "control-material-7b4d7.firebaseapp.com",
-  projectId: "control-material-7b4d7",
-  storageBucket: "control-material-7b4d7.firebasestorage.app",
-  messagingSenderId: "650193722396",
-  appId: "1:650193722396:web:69b06b63b083b06710d3d9"
+
 };
 
 // Inicializar Firebase
